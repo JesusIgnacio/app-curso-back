b5aeb3f57f0e3135927edab9ad269a7f5ebccef6
jesus.castillo@hites.cl <jesus.castillo@hites.cl>
2019-03-25 10:42:43 -0300

    apiservices test 01

--- services/api.service.js
+++ services/api.service.js
@@ -1,22 +1,6 @@
-"use strict";
+const express = require('express')
+const app = express()
 
-const ApiGateway = require("moleculer-web");
+app.get('/', (req, res) => res.send('test'))
 
-module.exports = {
-	name: "api",
-	mixins: [ApiGateway],
-
-	// More info about settings: http://moleculer.services/docs/moleculer-web.html
-	settings: {
-		port: process.env.PORT || 3000,
-
-		routes: [{
-			path: "/",
-			whitelist: [
-				// Access to any actions in all services
-				"*"
-			]
-		}]
-
-	}
-};
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
